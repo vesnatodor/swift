d3f3071573061d0599b4004d23074a4121fbad99
vesnatodor <vesnaatodorovic@gmail.com>
2023-07-13 15:25:04 +0200

    create single-product component

--- src/app/components/single/single.component.ts
+++ src/app/components/single/single.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/models/products.interface';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-single',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./single.component.css']
 })
 export class SingleComponent {
+
+  product: IProduct | undefined = {
+    id:0,
+    image:'',
+    title:'',
+    price:0,
+    basket:'',
+   quantity: 0
+  };
+  constructor (private _productsService: ProductsService,
+    private _route: ActivatedRoute,
+    ) {}
+
+ngOnInit():void{
+this._productsService.getProducts().subscribe((data: IProduct[])=> { 
+ this.product = data.find((el)=> el.id === +this._route.snapshot.params['id']);
+
+});
+
+}
 
 }
